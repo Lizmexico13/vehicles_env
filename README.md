f157659d169a8bfc965454a4ec5d144d9492981f
LizethL <l2ar_448@hotmail.com>
2025-07-15 23:23:32 -0600

    Agregue el histograma y el gráfico de dispersión

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vehicles_env
 Proyecto_sprint7
+Agregue un histograma con su botón para que se pueda ver
+agregue un gráfico de dispersión con su botón para que se pueda ver
+Agregue los dos checkboxes para que se puedan agregar el histograma y el gráfico de dispersión 
--- app.py
+++ app.py
@@ -2,6 +2,12 @@ import pandas as pd
 import plotly.graph_objects as go  # Importación de plotly.graph_objects como go
 import streamlit as st
 
+# titulo
+st.header("Proyecto 7 - Autos")
+
+# descrpición del proyecto
+st.write("**En las siguientes graficas podemos ver el los anuncios de los autos **")
+
 # Leer los datos del archivo CSV
 car_data = pd.read_csv('vehicles_us.csv')
 
@@ -11,7 +17,8 @@ hist_button = st.button('Construir histograma')
 # Lógica a ejecutar cuando se hace clic en el botón
 if hist_button:
     # Escribir un mensaje en la aplicación
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
     # Crear un histograma utilizando plotly.graph_objects
     # Se crea una figura vacía y luego se añade un rastro de histograma
@@ -19,6 +26,62 @@ if hist_button:
 
     # Opcional: Puedes añadir un título al gráfico si lo deseas
     fig.update_layout(title_text='Distribución del Odómetro')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
+
+# agregar el otro boton con la otra gráfica-
+plotly_chart = st.button('Construir diagrama dispersión')
+
+# Lógica a ejecutar cuando se hace clic en el botón
+if plotly_chart:
+    # Escribir un mensaje en la aplicación
+    st.write(
+        'Creación diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un disgrama de flujo utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de histograma
+    fig = go.Figure(
+        data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Relación entre Odómetro y Precio')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:  # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+
+    fig = go.Figure(
+        data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Relación entre Odómetro y Precio')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
+
+# crear una casilla de verificación
+plotly_chart = st.checkbox('Construir diagrama dispersión')
+if plotly_chart:
+    # Escribir un mensaje en la aplicación
+    st.write(
+        'Creación diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un disgrama de flujo utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de histograma
+    fig = go.Figure(
+        data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Relación entre Odómetro y Precio')
 
     # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
